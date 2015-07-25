d900347fb534660d09d6a2db9920e3bea0d857ec
Mônica Gracioli Marçal <monygracioli@gmail.com>
2015-07-25 17:21:16 -0300

    Exemplo simples de hello world.

---
+++ src/br/com/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.com.helloworld;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello World! : ) ");
+	}
+
+}
